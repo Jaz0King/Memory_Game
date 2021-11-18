af73f36c8748a51f8878960762277a5948ac2402
Jaziel Ramírez <89883165+Jaz0King@users.noreply.github.com>
2021-11-18 09:01:50 -0600

    Revert "actualización de efectos de sonido y música de fondo"

--- src/data.js
+++ src/data.js
@@ -1,24 +1,15 @@
-import {reScore} from "./app.js";
-//Pintar cartas dinámicamente en la "mesa" de juego
-let memorama = document.getElementById("mesa");
 
 //Traer la data de las cartas, del documento .json
-export let reStart = () => {
-    memorama.innerHTML = "";
-    reScore()
-    fetch("../data/memory.json")
-    .then((response) => response.json())
-    .then((data) => iterarCartas(data))
-    .catch((err) => console.log(err))
-}
+fetch("../data/memory.json")
+.then((response) => response.json())
+.then((data) => iterarCartas(data))
+.catch((err) => console.log(err))
 
-reStart();
-
+//Pintar cartas dinámicamente en la "mesa" de juego
+let memorama = document.getElementById("mesa");
 //Se iteran las cartas para pintarse dinámicamente
 let iterarCartas = (data) => {
     console.log(data.Memorama)
-    //Las cartas se revuelven cada que se carga el juego
-    data.Memorama.sort(() => Math.random() - 0.5);                                     
     for (const tarjeta of data.Memorama) {
         //console.log(tarjeta.Id)
         memorama.innerHTML += `<div class="card-container">
@@ -27,18 +18,10 @@ let iterarCartas = (data) => {
         <div class="front"></div>
         </div>
         </div>`;
+
         let caraURL = tarjeta.cara
         let portadaCard = document.getElementById(`${tarjeta.Id}`)
         portadaCard.style.backgroundImage = "url(" + caraURL + ")"
         portadaCard.style.backgroundSize = "cover"
-   
-    } 
+    }
 }
-
-
-
-
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -45,14 +45,15 @@ let playerID = null;
 
 =======
 //-----------//> Eventos DOM. Obtener los valores de búsqueda para procesarlos en data.js <//------------ //
-import {reStart} from "./data.js";
-
-
+let click = false;    //Activación de animación con click
+let turno = true;     //Turno de jugadores
+let player1 = 0;      //Contador para puntuación de jugadores
+let player2 = 0;
+let carta1;
+let id1;
+let carta2;
+let id2;
 let jugador1 = document.getElementById("jugador1")   //Pintado de los nombres del jugador en el html
-<<<<<<< HEAD
-let jugador2 = document.getElementById("jugador2") 
-
-=======
 let jugador2 = document.getElementById("jugador2")
 let puntos1 = document.getElementById("puntos1")
 let puntos2 = document.getElementById("puntos2")
@@ -165,31 +166,17 @@ window.matchCards = {
 }
                      //-----------//>Función para cambiar de pantalla de bienvenida<//-----------//
 document.getElementById("game").hidden= true;      //Pantalla de Bienvenida se muestra al cargar la página
->>>>>>> Release
 
-                         //-----------//>Función para cambiar de pantalla de bienvenida<//-----------//
-                         
-document.getElementById("game").hidden= true;      //Pantalla de Bienvenida se muestra al cargar la página
 let entrar = document.getElementById("btnInicio")  //La pantalla de Bienvenida se oculta al dar "click" al botón y se muestra el juego
 entrar.addEventListener("click", () => {
     document.getElementById("bienvenida").hidden = true
     document.getElementById("game").hidden = false
-    jugador1.innerHTML = "Jugador 1:  " + document.getElementById("nombre1").value 
-    jugador2.innerHTML = "Jugador 2:  " + document.getElementById("nombre2").value 
+    jugador1.innerHTML = "Jugador 1 : " + document.getElementById("nombre1").value 
+    jugador2.innerHTML = "Jugador 2 : " + document.getElementById("nombre2").value 
+   
 })
- 
 
-let reiniciar = document.getElementById("reiniciar")
-console.log(reiniciar);
-reiniciar.addEventListener("click", () => {
-   reStart(); 
-}) 
 
-let iniciar = document.getElementById("inicio")
-console.log(iniciar);
-iniciar.addEventListener("click", () => {
-   location.reload(); 
-}) 
 
 
 
